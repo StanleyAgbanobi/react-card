3ff4c347e0f56e646d7c6a0a50ddfec7deb3055a
Stanley.agbanobi <72680556+StanleyAgbanobi@users.noreply.github.com>
2024-07-31 14:12:47 +0000

    third commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Tweet from './Tweet';
 
+
 function App() {
+  const [users, setUsers] = useState([
+    { name='John', desc='Today with Elon musk', likes='19M' }
+    { name='Mark', desc='Protest in Nigeria 2024', likes='20k'}
+    { name='Maro', desc='Tech in 2025', likes='100k'}
+  ]);
+  
   return (
     <div className='app'>
+
     <h1>Hello React</h1>
-    <Tweet name='John' desc='Today with Elon' likes='19M' />
-    <Tweet name='Mark' desc='Protest in Nigeria' likes='20k' />
-    <Tweet name='Maro' desc='Tech in 2025' likes='100k' />
+    {users.map(user => (
+    <Tweet name={user.name} desc={user.desc} likes={user.likes} />
+    ))}
+
     </div>
-  );
+  )
+  
 }
+
 
 export default App;
